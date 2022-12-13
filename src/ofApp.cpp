af5f770513622d6f8bcbbb872f4e5ab525c4395c
jorjepadre <iamzhanaidar.mukanov@gmail.com>
2022-12-13 14:13:09 +0800

    WebCam Input and Edge Detection

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,17 +2,53 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    gui.setup();
+    gui.add(lowThresholdEdge.setup("Edge Threshold", 50, 0, 100));
+    webcamGrabber.setup(640, 640);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    webcamGrabber.update();
+    
+    // Edge Detection
+    if(webcamGrabber.isFrameNew()){
+        ofPixels pixs = webcamGrabber.getPixels();
+        webcamGrabberFrame.setFromPixels(pixs);
+        webcamGrabberFrameMat = toCv(webcamGrabberFrame);
+        cvtColor(webcamGrabberFrameMat, webcamGrabberFrameMatGray, CV_BGR2GRAY);
+        GaussianBlur(webcamGrabberFrameMatGray, webcamGrabberFrameMatGray, 3);
+        Canny(webcamGrabberFrameMatGray, webcamGrabberFrameMatEdge, lowThresholdEdge, lowThresholdEdge*3);
+    }
+    
+    // Circle Detection
+    
+//    if (webcamGrabber.isFrameNew()) {
+//        ofPixels pixs = webcamGrabber.getPixels();
+//        webcamGrabberFrame.setFromPixels(pixs);
+//        webcamGrabberFrameMat = toCv(webcamGrabberFrame);
+////        cvtColor(webcamGrabberFrameMat, webcamGrabberFrameMatGray, CV_BGR2GRAY);
+//        cvtColor(webcamGrabberFrameMat, webcamGrabberFrameMat, CV_BGR2GRAY);
+////        GaussianBlur(webcamGrabberFrameMatGray, webcamGrabberFrameMatGray, 3);
+//        GaussianBlur(webcamGrabberFrameMat, webcamGrabberFrameMat, 3);
+////        Canny(webcamGrabberFrameMatGray, webcamGrabberFrameMatEdge, lowThresholdEdge, lowThresholdEdge*2);
+//        Canny(webcamGrabberFrameMat, webcamGrabberFrameMat, lowThresholdEdge, lowThresholdEdge*2);
+//        HoughCircles(webcamGrabberFrameMat, circles, CV_HOUGH_GRADIENT, 2, 50, lowThresholdEdge*2, 100, 30, 50);
+//        ofSetColor(255, 0, 0);
+//    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofBackground(255);
+    ofSetColor(255, 255, 255);
+    
+    // Draw the input from the WebCam
+//    webcamGrabber.draw(0, 0, ofGetWidth(), ofGetHeight());
+    
+    // Draw the Edges from the WebCam Input
+    drawMat(webcamGrabberFrameMatEdge, 0, 0, ofGetWidth(), ofGetHeight());
+    gui.draw();
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxCv.h"
+#include "ofxGui.h"
+
+using namespace ofxCv;
+using namespace cv;
 
 class ofApp : public ofBaseApp{
 
@@ -20,5 +26,16 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+    ofVideoGrabber webcamGrabber;
+    ofImage webcamGrabberFrame;
+    Mat webcamGrabberFrameMat;
+    Mat webcamGrabberFrameMatGray;
+    Mat webcamGrabberFrameMatEdge;
+    
+    vector<Vec3f> circles;
+    
+    ofxPanel gui;
+    ofxIntSlider lowThresholdEdge;
 		
 };
